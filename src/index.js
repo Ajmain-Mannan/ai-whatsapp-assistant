7d6f0323458603c966a0bd051f3f482a244602d2
Ajmain-Mannan <ajmainmannan@gmail.com>
2026-06-28 23:40:27 +0100

    Integrate Gemini API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("AI Whatsappp Assistant Starting...");
+import ai from "./ai/gemini.js";
+
+async function main() {
+    const response = await ai.models.generateContent({
+         model: "gemini-2.5-flash",
+    contents: "Say hello to Ajmain in one sentence.",
+    })
+
+    console.log(response.text)
+}
+
+main();
